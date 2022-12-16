2f8aa0d8b8a1b5769f141418131910e96ab62c85
sarah 3 <109850449+kido6596@users.noreply.github.com>
2022-12-16 12:25:19 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const dropList = document.querySelectorAll(".drop-list select"),
 fromCurrency = document.querySelector(".from select"),
 toCurrency = document.querySelector(".to select"),
-apiKey =  'https://v6.exchangerate-api.com/v6/241dbb33bd253cf67002c0eb/latest/USD';
 getButton = document.querySelector("form button");
 
 for (let i = 0; i < dropList.length; i++) {
@@ -16,10 +15,36 @@ for (let i = 0; i < dropList.length; i++) {
         let optionTag = `<option value="${currency_code}" ${selected}>${currency_code}</option>`;
         dropList[i].insertAdjacentHTML("beforeend", optionTag);
     }
+    dropList[i].addEventListener("change", e =>{
+        loadFlag(e.target);
+    });
 }
+
+function loadFlag(element){
+    for(code in country_code){
+        if(code == element.value){
+            let imgTag = element.parentElement.querySelector("img");
+            imgTag.src = `https://flagcdn.com/48x36/${country_code[code].toLowerCase()}.png`
+        }
+    }
+}
+
+window.addEventListener("load", () =>{
+    getExchangeRate();
+});
 
 getButton.addEventListener("click", e =>{
     e.preventDefault();
+    getExchangeRate();
+});
+
+const exchangeIcon = document.querySelector(".drop-list .icon");
+exchangeIcon.addEventListener("click", ()=>{
+    let tempCode = fromCurrency.value;
+    fromCurrency.value = toCurrency.value;
+    toCurrency.value = tempCode;
+    loadFlag(fromCurrency);
+    loadFlag(toCurrency);
     getExchangeRate();
 });
 
@@ -31,6 +56,13 @@ function getExchangeRate(){
         amount.value = "1";
         amountVal = 1;
     }
-    let url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency.value}`;
-    fetch(url).then(response => console.log(response.json()))
+    exchangeRateTxt.innerText = "Getting exchange rate...";
+    let url = `https://v6.exchangerate-api.com/v6/${'241dbb33bd253cf67002c0eb'}/latest/${fromCurrency.value}`;
+    fetch(url).then(response => response.json()).then(result =>{
+        let exchangeRate = result.conversion_rates[toCurrency.value];
+        let totalExchangeRate = (amountVal * exchangeRate).toFixed(2);
+        const exchangeRateTxt = document.querySelector(".exchange-rate");
+        exchangeRateTxt.innerText = `${amountVal} ${fromCurrency.value} = ${totalExchangeRate} ${toCurrency.value}`
+
+    })  
 }
